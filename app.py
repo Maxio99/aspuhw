c239d72219a0ecff7c0eddf576e4324b22715bbf
Maxio99 <91377824+Maxio99@users.noreply.github.com>
2023-09-06 20:16:08 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import send_file, Flask, request
+import werkzeug
+from werkzeug import utils
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+app = Flask(__name__)
+# app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16 MB
+
+
+@app.route('/', methods=["POST", "GET"])
+def get_image():
+    try:
+        image_file = request.files['image']
+        filename = werkzeug.utils.secure_filename(image_file.filename)
+        file_path = "./uploaded/" + filename
+        print("\nReceived image File name : " + image_file.filename)
+        # TODO : Process the image and color it
+    except Exception:
+        return "Failed"
+    print("\n Image Sent : testing.png")
+    return send_file(path_or_file='./uploaded_images/testing.png', mimetype='image/png')
+
+
+if __name__ == "__main__":
+    app.run()
